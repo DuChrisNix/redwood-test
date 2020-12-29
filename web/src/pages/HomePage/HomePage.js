f57021153b74a11bd0aa2d66bb63c9ec9972fdda
Christian Kienast <fnatikk@gmail.com>
2020-12-29 01:17:43 +0100

    Add home page

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+const HomePage = () => {
+  return (
+    <>
+      <h1>HomePage</h1>
+      <p>
+        Find me in <code>./web/src/pages/HomePage/HomePage.js</code>
+      </p>
+    </>
+  )
+}
+
+export default HomePage
